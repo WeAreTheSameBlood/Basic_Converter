635792be5d406e6d2b56b847fdb1fc75969b2e53
Andrii <91153388+WeAreTheSameBlood@users.noreply.github.com>
2022-04-02 00:55:04 +0300

    Update classes and methods.

--- JavaModule/src/MainMenu.java
+++ JavaModule/src/MainMenu.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class MainMenu {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        Temperatures temperatures = new Temperatures();
 
-        System.out.println("Enter the temperature (in Celsius): ");
-        double inputTemp = scanner.nextDouble();
-
-        System.out.println("Convert to (enter a number): 1 - Kelvin, 2 - Fahrenheit");
+        System.out.println("What do yiu want to convert? \n1 - Temperatures; 2 - Distance;");
         int choice = scanner.nextInt();
-        switch (choice){
-            case 1 -> System.out.println(temperatures.toKelvin(inputTemp));
-            case 2 -> System.out.println(temperatures.toFahrenheit(inputTemp));
-            default -> System.out.println("Error");
+        switch (choice) {
+            case 1 -> new Temperatures().choiceTemperature();
+            case 2 -> new Distances();
         }
-
+        scanner.close();
     }
+
 }
+
+
+
--- JavaModule/src/Temperatures.java
+++ JavaModule/src/Temperatures.java
@@ -1,12 +1,37 @@
-public class Temperatures {
-    private double outputTemp;
+import java.util.Scanner;
 
-    String toKelvin(double inputTemp){
+public class Temperatures {
+    private double inputTemp, outputTemp;
+
+    public void choiceTemperature() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the temperature (in Celsius): ");
+        inputTemp = scanner.nextDouble();
+        menuT();
+        scanner.close();
+    }
+
+    private void menuT(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Convert to (enter a number): \n1 - Kelvin, 2 - Fahrenheit, 3 - Bach to menu.");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1 -> System.out.println(toKelvin(inputTemp));
+            case 2 -> System.out.println(toFahrenheit(inputTemp));
+            case 3 -> MainMenu.main(null);
+            default -> menuT();
+        }
+        scanner.close();
+    }
+
+
+
+    private String toKelvin(double inputTemp){
         outputTemp = 273.15 + inputTemp;
         return String.valueOf(outputTemp);
     }
 
-    String toFahrenheit(double inputTemp){
+    private String toFahrenheit(double inputTemp){
         outputTemp = (1.8 * inputTemp) + 32;
         return String.valueOf(outputTemp);
     }
