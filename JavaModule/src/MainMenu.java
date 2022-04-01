83d800c472fb6a3becc73f6d4121efbafffc9d27
Andrii <91153388+WeAreTheSameBlood@users.noreply.github.com>
2022-04-01 19:41:15 +0300

    Edited classes: MainMenu and Temperatures.

--- JavaModule/src/MainMenu.java
+++ JavaModule/src/MainMenu.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class MainMenu {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Temperatures temperatures = new Temperatures();
+
+        System.out.println("Enter the temperature (in Celsius): ");
+        double inputTemp = scanner.nextDouble();
+
+        System.out.println("Convert to (enter a number): 1 - Kelvin, 2 - Fahrenheit");
+        int choice = scanner.nextInt();
+        switch (choice){
+            case 1 -> System.out.println(temperatures.toKelvin(inputTemp));
+            case 2 -> System.out.println(temperatures.toFahrenheit(inputTemp));
+            default -> System.out.println("Error");
+        }
 
     }
 }
--- JavaModule/src/Temperatures.java
+++ JavaModule/src/Temperatures.java
@@ -1,2 +1,13 @@
 public class Temperatures {
+    private double outputTemp;
+
+    String toKelvin(double inputTemp){
+        outputTemp = 273.15 + inputTemp;
+        return String.valueOf(outputTemp);
+    }
+
+    String toFahrenheit(double inputTemp){
+        outputTemp = (1.8 * inputTemp) + 32;
+        return String.valueOf(outputTemp);
+    }
 }
